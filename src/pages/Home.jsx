cd0980e848f098c5789f15cf93dc013e58c93cd9
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-01-06 16:52:30 +0530

    added templates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import DefaultLayout from "../components/DefaultLayout";
+import template1 from "../resources/templates/template1.png";
+import template2 from "../resources/templates/template2.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const templates = [
+    {
+      title: "Resume 1",
+      image: template1,
+    },
+    {
+      title: "Resume 2",
+      image: template2,
+    },
+  ];
   return (
     <DefaultLayout>
-      <h1>This is Home Page</h1>
+      <div className="row home">
+        {templates.map((template, index) => {
+          return (
+            <div key={index} className="col-md-4 ">
+              <div className="template">
+                <img
+                  src={template.image}
+                  alt="template thumbnail"
+                  className="img-fluid"
+                />
+                <div className="text">
+                  <p>{template.title}</p>
+                  <button onClick={() => navigate(`/templates/${index + 1}`)}>
+                    TRY
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </DefaultLayout>
   );
 };
--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import { useParams } from "react-router-dom";
 import DefaultLayout from "../../components/DefaultLayout";
 import Template1 from "./Template1";
+import Template2 from "./Template2";
 
 const Templates = () => {
+  const params = useParams();
+
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  const getTemplate = () => {
+    switch (params.id) {
+      case "1": {
+        return <Template1 />;
+      }
+
+      case "2": {
+        return <Template2 />;
+      }
+
+      default: {
+        return <Template1 />;
+      }
+    }
+  };
   return (
     <DefaultLayout>
-      <Template1 />
+      <div className="d-flex justify-content-end my-5 gap-3 ">
+        <button className="py-2 px-3">Back</button>
+        <button className="py-2 px-3" onClick={handlePrint}>
+          Print
+        </button>
+      </div>
+      <div ref={componentRef}>{getTemplate()}</div>
     </DefaultLayout>
   );
 };
